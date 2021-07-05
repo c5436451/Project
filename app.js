ff16fe59d3be649d0964ada6308ce77e9530812e
c5436451 <mailtobiswajit@gmail.com>
2021-07-05 22:08:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,8 @@ app.post('/login', async (req, res) => {
     }
 });
 
+const port = process.env.PORT || 3000;
 
-server.listen(3000, function(){
-    console.log("Server is listening on port: 3000");
+server.listen(port, function(){
+    console.log("Server is listening on port: " + port  );
 });
